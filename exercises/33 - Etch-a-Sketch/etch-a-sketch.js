69465eed47dba0d9cbca6bc4a86d26da6dcc92ec
Andrew Nicholl <nichollandy@gmail.com>
2020-06-17 14:42:44 -0400

    #33 etch a sketch finished

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -1,26 +1,78 @@
 // Select the elements on the page - canvas, shake button
-const canvas = document.querySelector('#etch-a-sketh');
+const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
+const MOVE_AMOUNT = 10;
 
 // Setup our canvas for drawing
 // Make a variable called height and witdth from the same properties on our canvas
 const { width, height } = canvas;
-
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
 // Create random x and y starting points on the canvas
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = '10';
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.beginPath(); // Start the drawing
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
 
 // Write a draw function
+function draw({ key }) {
+  // Increment the hue
+  hue += 10;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  console.log(key);
+  // Start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // Move x and y values depending on what user did
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
 
 // Write a handler for the keys
-
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
 // Clear / shake function
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    'animationend',
+    function() {
+      canvas.classList.remove('shake');
+    },
+    { once: true }
+  );
+}
+
+shakeButton.addEventListener('click', clearCanvas);
 
 // Listen for arrow keys
+window.addEventListener('keydown', handleKey);
